cde1edff5e380920a60a05a2d5cdcb7f6eba0d99
Onyedikachi Ulelu <dirkaka@yahoo.com>
2023-12-20 20:15:39 +0000

    create alb file

--- variables.tf
+++ variables.tf
@@ -82,3 +82,16 @@ variable "region" {
   description = "create a standby db instance"
   type        = bool
   }
+
+  # acm variables
+  variable "domain_name" {
+  description = "domain name"
+  type        = string
+  }
+
+  variable "alternative_names" {
+  description = "sub domain name"
+  type        = string
+  }
+
+
