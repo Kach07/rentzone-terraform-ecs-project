cc8128cfcfd02a3d0a7d5aec525777743e040130
Onyedikachi Ulelu <dirkaka@yahoo.com>
2024-01-13 01:58:42 +0000

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "kach07-terraform-remote-state2"
+    bucket         = "kach07-terrafor-remote-state2"
     key            = "rentzone-ecs/terraform.tfstate"
     region         = "eu-west-2"
     profile        = "Mr.Kachy"
